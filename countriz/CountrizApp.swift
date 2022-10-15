050a3c54e15f2fb246d39e7f69481abcd572b267
Sengsathit <sengsathit@gmail.com>
2022-10-15 10:17:37 +0200

    Initial Commit

---
+++ countriz/CountrizApp.swift
@@ -0,0 +1,17 @@
+//
+//  countrizApp.swift
+//  countriz
+//
+//  Created by Sengsathit SILALAK on 15/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CountrizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
